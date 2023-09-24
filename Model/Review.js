4f21d5b235700b891e04d3013206132a778368c1
Shaolong Li <shaolongli26@gmail.com>
2023-09-24 12:33:25 -0700

    re vert "set up schema and resolvers"
    This reverts commit 056af81114f63512a2c58b92bb5b4278fb2fd0f4.

--- Model/Review.js
+++
@@ -1,6 +0,0 @@
-const { model, Schema } = require('mongoose')
-
-const reviewSchema= new Schema({
-   body: String
-})
-module.exports = model('Review', reviewSchema)
--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    MONGODB: 'mongodb+srv://shaolongli26:y77eSsptXhnKwBx@cluster0.bpftuyj.mongodb.net/movieDBGraphql?retryWrites=true&w=majority',
-    SECRET_KEY: 'do not share'
-}
